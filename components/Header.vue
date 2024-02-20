31ee47bbe6a529a5abe8ac9a538570c066d36dfd
satish6664 <satish1242@sasi.ac.in>
2024-02-20 18:01:09 +0530

    Changing the style

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="bg-slate-600 text-white flex justify-around align-center mb-4 h-10">
+    <header class="bg-slate-600 text-white flex justify-around items-center mb-4 h-10">
         <NuxtLink to="/">Home</NuxtLink> &nbsp;
         <NuxtLink to="/about">About</NuxtLink>
     </header>
